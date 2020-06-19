82f88612a328234adae799eccb7ae8fb95a1819b
vladov3000 <vlad@instance-5.us-central1-a.c.rasa-280223.internal>
2020-06-19 04:06:12 +0000

    tutorial almost complete, with a few bugs

--- actions.py
+++ actions.py
@@ -1,27 +1,50 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/core/actions/#custom-actions/
+from typing import Text, List, Dict, Any, Union
 
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.forms import FormAction
+from rasa_sdk.events import UserUtteranceReverted
 
-# This is a simple example for a custom action which utters "Hello World!"
+class ActionGreetUser(Action):
+    """Revertible mapped action for utter_greet"""
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+    def name(self):
+        return "action_greet"
+
+    def run(self, dispatcher, tracker, domain):
+        dispatcher.utter_template("utter_greet", tracker)
+        return [UserUtteranceReverted()]
+
+class SalesForm(FormAction):
+        """Collects sales information and adds it to the spreadsheet"""
+
+        def name(self):
+             return "sales_form"
+
+        @staticmethod 
+        def required_slots(tracker): 
+            return [ 
+                    "job_function", 
+                    "use_case", 
+                    "budget", 
+                    "person_name", 
+                    "company", 
+                    "business_email", 
+                    ]
+
+        def submit(
+                self, 
+                dispatcher: CollectingDispatcher,
+                tracker: Tracker,
+                domain: Dict[Text, Any],
+            ) -> List[Dict]:
+            dispatcher.utter_message("Thanks for getting in touch, we’ll contact you soon")
+            return []
+
+        def slot_mappings(self) -> Dict[Text, Union[Dict, List[Dict[Text, Any]]]]:
+            """A dictionary to map required slots to
+            - an extracted entity
+            - intent: value pairs
+            - a whole message
+            or a list of them, where a first match will be picked"""
+            return {"use_case": self.from_text(intent="inform")}
--- data/stories.md
+++ data/stories.md
@@ -13,3 +13,41 @@
 ## Some question from FAQ
 * faq
     - respond_faq
+
+## sales form
+* contact_sales
+    - sales_form                   <!--Run the sales_form action-->
+    - form{"name": "sales_form"}   <!--Activate the form-->
+    - form{"name": null}           <!--Deactivate the form-->
+
+## just sales, continue
+* contact_sales
+    - sales_form
+    - form{"name": "sales_form"}
+* faq
+    - respond_faq
+    - sales_form
+    - form{"name": null}
+
+## explain email
+* contact_sales
+    - sales_form
+    - form{"name": "sales_form"}
+    - slot{"requested_slot": "business_email"}
+* explain
+    - utter_explain_why_email
+    - sales_form
+    - form{"name": null}
+
+## explain budget
+* contact_sales
+    - sales_form
+    - form{"name": "sales_form"}
+    - slot{"requested_slot": "budget"}
+* explain
+    - utter_explain_why_budget
+    - sales_form
+    - form{"name": null}
+
+* out_of_scope
+  utter_out_of_scope
