7617acec14a50996da96ca236a61bb7646bda899
Your Name <you@example.com>
2020-07-13 04:06:35 +0000

    working on saving slots after sales form

--- actions.py
+++ actions.py
@@ -1,9 +1,12 @@
 from typing import Text, List, Dict, Any, Union
+import time
+from ruamel import yaml
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.forms import FormAction
 from rasa_sdk.events import UserUtteranceReverted
+
 
 class ActionGreetUser(Action):
     """Revertible mapped action for utter_greet"""
@@ -15,36 +18,41 @@ class ActionGreetUser(Action):
         dispatcher.utter_template("utter_greet", tracker)
         return [UserUtteranceReverted()]
 
+
 class SalesForm(FormAction):
-        """Collects sales information and adds it to the spreadsheet"""
+    """Collects sales information and adds it to the spreadsheet"""
 
-        def name(self):
-             return "sales_form"
+    def name(self):
+        return "sales_form"
 
-        @staticmethod 
-        def required_slots(tracker): 
-            return [ 
-                    "job_function", 
-                    "use_case", 
-                    "budget", 
-                    "person_name", 
-                    "company", 
-                    "business_email", 
-                    ]
+    @staticmethod
+    def required_slots(tracker):
+        return [
+                "job_function",
+                "use_case",
+                "budget",
+                "person_name",
+                "company",
+                "business_email",
+            ]
 
-        def submit(
-                self, 
+    def submit(
+                self,
                 dispatcher: CollectingDispatcher,
                 tracker: Tracker,
                 domain: Dict[Text, Any],
             ) -> List[Dict]:
-            dispatcher.utter_message("Thanks for getting in touch, we’ll contact you soon")
-            return []
+        print(f"Result Slot Values: {tracker.current_slot_values()}")
+        dispatcher.utter_message(
+        "Thanks for getting in touch, we’ll contact you soon")
+        with open(f'saves/saved_data{time.time()}.yml', 'w+') as save_file:
+            yaml.dump(tracker.current_slot_values())
+        return []
 
-        def slot_mappings(self) -> Dict[Text, Union[Dict, List[Dict[Text, Any]]]]:
-            """A dictionary to map required slots to
-            - an extracted entity
-            - intent: value pairs
-            - a whole message
-            or a list of them, where a first match will be picked"""
-            return {"use_case": self.from_text(intent="inform")}
+    def slot_mappings(self) -> Dict[Text, Union[Dict, List[Dict[Text, Any]]]]:
+        """A dictionary to map required slots to
+        - an extracted entity
+        - intent: value pairs
+        - a whole message
+        or a list of them, where a first match will be picked"""
+        return {"use_case": self.from_text(intent="inform")}
