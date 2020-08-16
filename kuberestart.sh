a0b41619c861c1a12227fa8bbfade8927b860676
vladov3000 <vladov3000@gmail.com>
2020-08-16 17:53:23 +0000

    fixed nginx kubernete bug

--- kuberestart.sh
+++ kuberestart.sh
@@ -1,4 +1,5 @@
 kubectl delete --all pods
 kubectl delete --all deployments
 kubectl get configmap action-config -o yaml > kuberes/action-config.yml
+kubectl get configmap nginx-config -o yaml > kuberes/nginx-config.yml
 kubectl apply -f kuberes
